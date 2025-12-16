0a5011d1cd1e5060226e9bb0b965e51a5607fbc3
XxdrixX <daveranola351@gmail.com>
2025-12-16 18:43:13 +0000

    signup validation

---
+++ app/lib/validation/signup.ts
@@ -0,0 +1,16 @@
+import * as z from "zod";
+
+export const SignupFormSchema = z.object({
+    name: z.string().min(2, { message: 'Name must be at least 2 characters.' }),
+    email: z.email({ message: 'Please enter a valid email.' }),
+    password: z
+    .string()
+    .min(8, { message: 'Be at least 8 characters long.' })
+    .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
+    .regex(/[0-9]/, { message: 'Contain at least one number.' })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: 'Contain at least one special character.',
+    }),
+});
+
+export type SignupValues = z.infer<typeof SignupFormSchema>;
